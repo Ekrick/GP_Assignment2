b6d76816ab92dc7db0c2d0e0c99e1406057aa9d8
Ekrick <113420224+Ekrick@users.noreply.github.com>
2022-11-23 14:04:35 +0100

    Changed stuff

--- Assets/Scripts/WordCompare.cs
+++ Assets/Scripts/WordCompare.cs
@@ -6,6 +6,7 @@ public class WordCompare : MonoBehaviour
 {
     [SerializeField] private string _targetTest = "testa";
     [SerializeField] private string _guessTest = "testa";
+    List<char> _triedLetters = new List<char>();
     List<char> _targetList = new List<char>();
     List<char> _guessList = new List<char>();
 
@@ -13,71 +14,65 @@ public class WordCompare : MonoBehaviour
     {
         _targetList = StringToCharList(_targetTest);
         _guessList = StringToCharList(_guessTest);
-        foreach (char c in _targetList)
-        {
-            Debug.Log(c);
-        }
-        foreach (char c in _guessList)
-        {
-            Debug.Log(c);
-        }
+        Compare(_guessList, _targetList);
     }
 
+
+    //private char[] StringToCharArray(string word)
+    //{
+    //    char[] array = new char[5];
+    //    for (int i = 0; i < word.Length; i++)
+    //    {
+    //        array[i] = word[i];
+    //    }
+    //    return array;
+    //}
     private List<char> StringToCharList(string word)
     {
         List<char> list = new List<char>();
         list.AddRange(word);
         return list;
     }
-
-    public void BigCompare(string target, string guess)
+    private void FirstCompare(List<char> guess, List<char> target)
     {
-        List<int> indexList = MatchingIndexList(target, guess);
-        string t = target;
-        string g = guess;
-        int count = 0;
-        foreach (int i in indexList)
-        {
-            int indx = i - count;
-            t = t.Remove(indx, 1);
-            g = g.Remove(indx, 1);
-            count++;
-        }
-        foreach (char c in t)
-        {
-            SmallCompare(c, g);
-        }
-
-
-    }
-
-    private List<int> MatchingIndexList(string target, string guess)
-    {
-        List<int> indexList = new List<int>();
-        for (int i = 0; i < target.Length; i++)
+        for (int i = 0; i < target.Count; i++)
         {
             if (target[i] == guess[i])
             {
-                Debug.Log("Rätt" + guess[i]);
-                indexList.Add(i);
+                Debug.Log("Rätt: " + guess[i]);
+                _triedLetters.Add(target[i]);
+                target[i] = '+';
+                guess[i] = '-';
             }
         }
-        return indexList;
     }
-
-
-
-    private void SmallCompare(char target, string guess)
+    private void SecondCompare(List<char> guess, List<char> target)
     {
-        bool temp = false;
-        for (int i = 0; i < guess.Length; i++)
+        foreach (char c in guess)
         {
-            if (target == guess[i])
+            SmallCompare(c, target);
+        }
+    }
+    private void SmallCompare(char guess, List<char> target)
+    {
+        for (int i = 0; i < target.Count; i++)
+        {
+            if (guess == target[i])
             {
-                Debug.Log("Nästan rätt" + target);
+                target[i] = '+';
+                Debug.Log("Nästan rätt: " + guess);
                 break;
             }
         }
+    }
+
+    public void Compare(List<char> guess, List<char> target)
+    {
+        List<char> guessList = guess;
+        List<char> targetList = target;
+        FirstCompare(guessList, targetList);
+        SecondCompare(guessList, targetList);
+
     }
 
 }
